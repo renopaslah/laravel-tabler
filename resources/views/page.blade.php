11d3f6d1531b264febab1d4b891847b2c521ae97
=Reno Paslah <reno.nilam@gmail.com>
2023-06-15 20:27:04 +0700

    pilot

---
+++ resources/views/page.blade.php
@@ -0,0 +1,15 @@
+@extends('template')
+
+@section('subtitle')
+    <x-subtitle />
+@endsection
+
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                Blank
+            </div>
+        </div>
+    </div>
+@endsection
